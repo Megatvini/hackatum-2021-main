322bd2e96767af4c355b3bbd2a0cac94772bf22b
Nika <ndogh13@freeuni.edu.ge>
2021-11-20 03:26:10 +0400

    Implement simple etherium balances

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -8,6 +8,8 @@ contract Bank is IBank {
     address priceOracle;
     address hakToken;
 
+    mapping (address => uint256) public ethBalances;
+
     constructor(address _priceOracle, address _hakToken) {
         priceOracle = _priceOracle;
         hakToken = _hakToken;
@@ -15,6 +17,8 @@ contract Bank is IBank {
 
     function deposit(address token, uint256 amount) payable external override returns (bool) {
             if (token == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
+                ethBalances[msg.sender] += msg.value;
+                emit Deposit(msg.sender, token, msg.value);
                 return true;
             } else if (token == hakToken) {
                 return true;
@@ -51,9 +55,13 @@ contract Bank is IBank {
         override
         returns (uint256) {}
 
-    function getBalance(address token)
-        view
-        public
-        override
-        returns (uint256) {}
+    function getBalance(address token) view public override returns (uint256) {
+        if (token == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
+            return ethBalances[msg.sender];
+        } else if (token == hakToken) {
+            return 0;
+        } else {
+            revert("token not supported");
+        }
+    }
 }

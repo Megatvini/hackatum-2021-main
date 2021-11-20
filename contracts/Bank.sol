4c80b70ffd4248e761ce6a27833ba9e990836596
Nika <ndogh13@freeuni.edu.ge>
2021-11-20 09:22:48 +0400

    Update storage structs

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,25 +6,24 @@ import "./interfaces/IPriceOracle.sol";
 import "./interfaces/IERC20.sol";
 
 
+
 contract Bank is IBank {
     address priceOracle;
     address hakToken;
     address ethToken;
     address selfAddress;
 
-    struct AccountInfo {
-
-        uint256 ethBalance;
-        uint256 ethInterest;
-        uint ethLastBlockNumber;
-
-        uint256 hakBalance;
-        uint256 hakInterest;
-        uint hakLastBlockNumber;
-
+    struct CurrencyBalance {
+        uint256 balance;
+        uint256 interest;
+        uint lastBlockNumber;
+        uint256 interestRate;
     }
 
-    mapping(address => AccountInfo) accountsInfo;
+
+    mapping(address => CurrencyBalance) ethDeposits;
+    mapping(address => CurrencyBalance) ethLoans;
+    mapping(address => CurrencyBalance) hakDeposits;
 
 
     constructor(address _priceOracle, address payable _hakToken) {
@@ -34,17 +33,41 @@ contract Bank is IBank {
         selfAddress = address(this);
     }
 
+    function calculateSimpleInterest(CurrencyBalance storage currency, uint curBlockNumber) private view returns(uint256) {
+        return (curBlockNumber - currency.lastBlockNumber) * currency.interestRate * currency.balance / 100 / 100; // 100 blocks 3 percent
+    }
+
+    function updateCurrencyInterest(CurrencyBalance storage currency, uint curBlockNumber) private {
+        currency.interest += calculateSimpleInterest(currency, curBlockNumber);
+    }
+
+    function addBalance(CurrencyBalance storage currency, uint256 amount, uint curBlockNumber) private {
+        updateCurrencyInterest(currency, curBlockNumber);
+        currency.balance += amount;
+        currency.lastBlockNumber = curBlockNumber;
+    }
+
+    function removeBalanceAndInterest(CurrencyBalance storage currency, uint256 amount, uint curBlockNumber) private returns(uint256) {
+        updateCurrencyInterest(currency, curBlockNumber);
+        
+        uint256 totalRemoved = amount + currency.interest;
+        currency.balance -= amount;
+        currency.interest = 0;
+        currency.lastBlockNumber = curBlockNumber;
+        return totalRemoved;
+    }
+
+    function getTotalBalance(CurrencyBalance storage currency, uint256 curBlockNumber) private view returns(uint256) {
+        return currency.balance + currency.interest + calculateSimpleInterest(currency, curBlockNumber);
+    } 
+
     function deposit(address token, uint256 amount) payable external override returns (bool) {
         if (token == ethToken) {
-            accountsInfo[msg.sender].ethInterest = getEthTotalInterest(msg.sender, block.number);
-            accountsInfo[msg.sender].ethLastBlockNumber = block.number;
             // TODO find out when to revert, amount == 0 ?
-            accountsInfo[msg.sender].ethBalance += msg.value <= amount ? msg.value : amount;
-
+            addBalance(ethDeposits[msg.sender], msg.value <= amount ? msg.value : amount, block.number);
             emit Deposit(msg.sender, token, msg.value);
             return true;
         } else if (token == hakToken) {
-
             // TODO steal later
 
             uint256 allowance = IERC20(hakToken).allowance(msg.sender, selfAddress);
@@ -59,10 +82,7 @@ contract Bank is IBank {
                 revert("unsuccessful transfer from");
             }
 
-            accountsInfo[msg.sender].hakInterest = getHakTotalInterest(msg.sender, block.number);
-            accountsInfo[msg.sender].hakLastBlockNumber = block.number;
-            accountsInfo[msg.sender].hakBalance += amount;
-
+            addBalance(hakDeposits[msg.sender], amount, block.number);
             emit Deposit(msg.sender, token, amount);
             return true;
         } else {
@@ -70,62 +90,47 @@ contract Bank is IBank {
         }
     }
 
-    function getEthTotalInterest(address customerAddress, uint currentBlockNumber) private view returns (uint256) {
-        return (currentBlockNumber - accountsInfo[customerAddress].ethLastBlockNumber) * 3 * accountsInfo[customerAddress].ethBalance / 10000 + accountsInfo[customerAddress].ethInterest;
-    }
-
-    function getHakTotalInterest(address customerAddress, uint currentBlockNumber) private view returns (uint256) {
-        return (currentBlockNumber - accountsInfo[customerAddress].hakLastBlockNumber) * 3 * accountsInfo[customerAddress].hakBalance / 10000 + accountsInfo[customerAddress].hakInterest;
-    }
-
     function withdraw(address token, uint256 amount) external override returns (uint256) {
         if (token == ethToken) {
-            if (accountsInfo[msg.sender].ethBalance == 0) {
+            CurrencyBalance storage currency = ethDeposits[msg.sender];
+
+            if (currency.balance == 0) {
                 revert("no balance");
             }
 
-            if (accountsInfo[msg.sender].ethBalance < amount) {
+            if (currency.balance < amount) {
                 revert("amount exceeds balance");
             }
 
             if (amount == 0) {
-                amount = accountsInfo[msg.sender].ethBalance;
+                amount = currency.balance;
             }
 
-            uint256 totalInterest = getEthTotalInterest(msg.sender, block.number);
+            uint256 totalAmount = removeBalanceAndInterest(currency, amount, block.number);
+            msg.sender.transfer(totalAmount);
+            emit Withdraw(msg.sender, token, totalAmount);
 
-            accountsInfo[msg.sender].ethLastBlockNumber = block.number;
-            accountsInfo[msg.sender].ethBalance -= amount;
-            accountsInfo[msg.sender].ethInterest = 0;
-
-            msg.sender.transfer(amount + totalInterest);
-            emit Withdraw(msg.sender, token, amount + totalInterest);
-
-            return amount + totalInterest;
+            return totalAmount;
         } else if (token == hakToken) {
-            if (accountsInfo[msg.sender].hakBalance == 0) {
+            CurrencyBalance storage currency = hakDeposits[msg.sender];
+
+            if (currency.balance == 0) {
                 revert("no balance");
             }
 
-            if (accountsInfo[msg.sender].hakBalance < amount) {
+            if (currency.balance < amount) {
                 revert("amount exceeds balance");
             }
 
             if (amount == 0) {
-                amount = accountsInfo[msg.sender].hakBalance;
+                amount = currency.balance;
             }
 
-            uint256 totalInterest = getHakTotalInterest(msg.sender, block.number);
+            uint256 totalAmount = removeBalanceAndInterest(currency, amount, block.number);
+            IERC20(hakToken).approve(msg.sender, totalAmount);
 
-            accountsInfo[msg.sender].hakLastBlockNumber = block.number;
-            accountsInfo[msg.sender].hakBalance -= amount;
-            accountsInfo[msg.sender].hakInterest = 0;
-
-            IERC20(hakToken).approve(msg.sender, amount + totalInterest);
-
-            emit Withdraw(msg.sender, token, amount + totalInterest);
-
-            return amount + totalInterest;
+            emit Withdraw(msg.sender, token, totalAmount);
+            return totalAmount;
         } else {
             revert("token not supported");
         }
@@ -135,8 +140,6 @@ contract Bank is IBank {
         if (token != ethToken) {
             revert();
         }
-
-//        uint x = (deposits[account] + accruedInterest[account]) * 10000 / (borrowed[account] + owedInterest[account]) >= 15000;
 
         return 0;
     }
@@ -161,9 +164,9 @@ contract Bank is IBank {
 
     function getBalance(address token) view public override returns (uint256) {
         if (token == ethToken) {
-            return accountsInfo[msg.sender].ethBalance + getEthTotalInterest(msg.sender, block.number);
+            return getTotalBalance(ethDeposits[msg.sender], block.number);
         } else if (token == hakToken) {
-            return accountsInfo[msg.sender].hakBalance + getHakTotalInterest(msg.sender, block.number);
+            return getTotalBalance(hakDeposits[msg.sender], block.number);
         } else {
             revert("token not supported");
         }
